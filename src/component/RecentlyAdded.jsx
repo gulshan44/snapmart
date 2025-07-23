6bad599b6cd43b0e4f3418b1b85be6aa5f216813
Gulshan Verma <gulshanrock4477@gmail.com>
2025-07-23 18:40:30 +0530

    cart ui update

--- src/component/RecentlyAdded.jsx
+++ src/component/RecentlyAdded.jsx
@@ -40,7 +40,7 @@ const RecentlyAdded = () => {
 
                 </div>
 
-                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-6 my-5'>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-2 sm:gap-6 my-5'>
                     <ProductBox products={products} />
                 </div>
 
